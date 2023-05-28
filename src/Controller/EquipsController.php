df635c6b9ba85db3fc56f5b3e6d2b8061e01236b
jaume tuset <tusetjaume7@gmail.com>
2023-05-29 01:35:56 +0200

    Commit Twig EquipsJaume

--- src/Controller/EquipsController.php
+++ src/Controller/EquipsController.php
@@ -1,10 +1,11 @@
 <?php
 
 namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class EquipsController{
+class EquipsController extends AbstractController{
     private $equips = array( 
         array("codi" => "1", "nom" => "Equip Roig", "cicle" => "DAW", "curs" => "22/23", "membres" => array("David","Alejandro","Jose","Marta")),  
         array("codi" => "2", "nom" => "Equip Groc", "cicle" => "DAM", "curs" => "21/22", "membres" => array("Alvaro","Ivan","Marcos","Samuel")),
@@ -12,24 +13,17 @@ class EquipsController{
         array("codi" => "4", "nom" => "Equip Verd", "cicle" => "REDES", "curs" => "18/19","membres" => array("Yasmine","Valentino","Mario","Samantha")),  
     );
 
-    #[Route('/equip/{codi}' ,name:'dades_equip')]
+    #[Route('/equip/{codi<\d+>?1}',name:'dades_equips', requirements: ['codi' => '\d+'])]
     public function dades($codi){
-        $resultat = array_filter($this->equips,
-        function($dades) use ($codi){
-            return $dades["codi"]==$codi;
+        
+        $resultat=array_filter($this->equips, 
+        function($equip) use ($codi){
+            return $equip["codi"] == $codi;
         });
         if(count($resultat)>0){
-            $resposta = "";
-            $resultat = array_shift($resultat);
-            $mb = implode(" ",$resultat["membres"]);
-            $resposta .= "<ul><li>" . $resultat["nom"] . "</li>" . 
-            "<li>" . $resultat["cicle"] . "</li>" .
-            "<li>" . $resultat["curs"] . "</li>" .
-            "<li>" . $mb ."</li></ul>";
-
-            return new Response("<html><body>$resposta</body></html>");
+            return $this->render('dades_equip.html.twig',array('equip'=>array_shift($resultat)));
         }else{
-            return new Response("No s'ha trobatl'equip : ". $codi);
+            return $this->render('dades_equip.html.twig',array('equip'=> NULL));
         }
     }
 }
